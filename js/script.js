782d49f2dded21f4ea49f6f6c4418ad360e22057
Anggita <anggitagita07@gmail.com>
2025-09-26 20:13:25 +0800

    ubah isi index.html, css, dan js

--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ function WelcomeMessage() {
 
 // Form Validation
 function validateForm() {
+    let form = document.getElementById("form");
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
@@ -21,9 +22,20 @@ function validateForm() {
    if (name === "" || email === "" || message === "") {
     // Display an error message
     alert("Please fill in all fields");
+    return;
    }
    else {
     // Display a success message
     alert(`Thanks, ${name}! Form submitted successfully!`);
    }
+
+   //Display Message Us
+document.getElementById("currentTime").textContent = new Date().toString();
+document.getElementById("resName").textContent = name;
+document.getElementById("resEmail").textContent = email;
+document.getElementById("resMessage").textContent = message;
+
+document.getElementById("resultBox").classList.remove("hidden");
+
+form.reset();
 }
